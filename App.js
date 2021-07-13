486ab0b5d1c49efd40b520bf8aae27bc7cc60f55
hardik-kh <hardik.eldeco@gmail.com>
2021-07-13 16:55:56 +0530

    todo app by using react native

--- App.js
+++ App.js
@@ -1,13 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList, Alert,ScrollView } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
+  const [todos,setTodos] = useState([
+    {text:'buy coffee',key:'1'},
+    {text:'To make an app',key:'2'},
+    {text:'play on the switch',key:'3'},
+  ]);
+
+  const pressHandler = (key) => {
+    
+    setTodos((prevTodos) =>{
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) =>{
+
+    if(text.length >= 3){
+      setTodos((prevTodos) =>{
+        return[
+          {text: text,key: Math.random().toString()},
+          ...prevTodos
+        ];
+  
+      });
+
+    }
+    else{
+      Alert.alert('OOPS!','Todos must contain atleast 3 chars',[
+        {text:'OK',onPress: () => console.log('Alert Dismissed') }  
+         ])
+    }
+
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler}/>
+        <View style={styles.list}>
+          <FlatList 
+          data={todos}
+          renderItem={({ item }) => (
+            <TodoItem item={item} pressHandler={pressHandler}/>
+          )}
+          />
+        </View>
+      </View>
     </View>
+
   );
 }
 
@@ -15,7 +63,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+  content:{
+    padding:40
+  },
+  list: {
+    marginTop:20
+  }
+
+
 });
---
+++ components/todoItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View,StatusBar,ScrollView } from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons'
+
+export default function TodoItem({item, pressHandler }){
+    return(
+    
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+        <StatusBar barStyle='light-content'/>
+        <View style={styles.item}>
+            <MaterialIcons name='delete' size={22}/>
+        <Text style={styles.itemText}>{item.text}</Text>
+
+        </View>
+        
+    </TouchableOpacity>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    item:{
+        padding: 16,
+        marginTop: 16,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        borderRadius: 10,
+        fontWeight:'bold',
+        flexDirection:'row'
+    },
+    itemText:{
+        marginLeft:10,
+        fontSize:18
+    }
+});
